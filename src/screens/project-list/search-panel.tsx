d352c58642c5e3d59587b82016f851ce9ce07b3a
jade <mayuhome@163.com>
2024-05-22 16:13:12 +0800

    js to ts

---
+++ src/screens/project-list/search-panel.tsx
@@ -0,0 +1,50 @@
+// 定义search panel interface
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+}
+
+export interface User {
+  id: number;
+  name: string;
+}
+
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
+  // 渲染页面
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (v) => (v === 0 ? false : !v);
+export const isFalsy = (v: any) => (v === 0 ? false : !v);
 
-export const cleanObject = (obj) => {
-  const result = { ...obj };
+export const cleanObject = (obj: object) => {
+  const result: any = { ...obj };
   Object.keys(result).forEach((key) => {
     if (isFalsy(result[key])) {
       delete result[key];
@@ -12,13 +12,13 @@ export const cleanObject = (obj) => {
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: Function) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     const timer = setTimeout(() => {
